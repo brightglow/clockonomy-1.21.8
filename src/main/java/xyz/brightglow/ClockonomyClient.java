07be30aeabee5c4087d06dfde6038bb616158d60
michael <rwxmichael@gmail.com>
2025-08-06 22:17:28 -0400

    Initial commit

---
+++ src/main/java/xyz/brightglow/ClockonomyClient.java
@@ -0,0 +1,11 @@
+package xyz.brightglow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClockonomyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/xyz/brightglow/ClockonomyDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.brightglow;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ClockonomyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
