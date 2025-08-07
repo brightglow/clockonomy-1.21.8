a2f11ed03a7767edf0ed92b15d26bdeacf9d1790
michael <rwxmichael@gmail.com>
2025-08-06 22:18:37 -0400

    added test comment

--- src/main/java/xyz/brightglow/ClockonomyDataGenerator.java
+++ src/main/java/xyz/brightglow/ClockonomyDataGenerator.java
@@ -3,6 +3,7 @@ package xyz.brightglow;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+//test change
 public class ClockonomyDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
